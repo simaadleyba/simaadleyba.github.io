ab7ae460f0fd68f51bd6d3ff04d2587ee64f823d
sima <99277649+simaadleyba@users.noreply.github.com>
2026-03-14 15:36:05 +0300

    personalized webpage

---
+++ assets/photos.py
@@ -0,0 +1,116 @@
+[
+    {
+        'name': 'DSCF2459',
+        'title': 'two chairs in middle of nowhere',
+        'description': 'two chairs in middle of nowhere. idk why.',
+        'location': 'near kocaeli'
+    },
+    {
+        'name': 'DSCF2617',
+        'title': 'amca',
+        'description': 'amca giving classic turkish pose.',
+        'loacation': 'near bursa'
+    },
+    {
+        'name': 'DSCF2858',
+        'title': 'vapurda',
+        'description': 'a turkish man looking away in the ship',
+        'location': 'kadikoy, istanbul'
+    },
+    {
+        'name': 'DSCF3178',
+        'title': 'abstract',
+        'description': "people in museum, they're always on move",
+        'location': 'istanbul museum of modern arts'
+    },
+    {
+        'name': 'DSCF3201',
+        'title': 'museum',
+        'description': 'museum of modern arts',
+        'location': 'istanbul museum of modern arts'
+    },
+    {
+        'name': 'DSCF3245',
+        'title': 'two guys in antique market',
+        'description': 'two guys in antique market. nothing more. maybe a cigarette?',
+        'location': 'ferikoy, istanbul'
+    },
+    {
+        'name': 'DSCF3252',
+        'title': 'authentic street food by teyzeler',
+        'description': "teyzeler means 'aunts' in turkish. and they were preparing a food. i don't know what. i wasn't interested.",
+        'location': 'ferikoy, istanbul'
+    },
+    {
+        'name': 'DSCF3325',
+        'title': 'bank',
+        'description': 'sabanci in winter is something else. listening to jeon jin hee is highly recommended',
+        'location': 'sabanci university, istanbul'
+    },
+    {
+        'name': 'DSCF3883',
+        'title': 'offtown_1',
+        'description': 'photo from offtown festival of sabanci university.',
+        'location': 'sabanci university, istanbul'
+    },
+    {
+        'name': 'DSCF3918',
+        'title': 'offtown_2',
+        'description': 'photo from offtown festival of sabanci university.',
+        'location': 'sabanci university, istanbul'
+    },
+        {
+        'name': 'DSCF3953',
+        'title': 'offtown_3',
+        'description': 'photo from offtown festival of sabanci university.',
+        'location': 'sabanci university, istanbul'
+    },
+        {
+        'name': 'DSCF4003',
+        'title': 'offtown_4',
+        'description': 'photo from offtown festival of sabanci university.',
+        'location': 'sabanci university, istanbul'
+    },
+    {
+        'name': 'DSCF4848',
+        'title': 'fish',
+        'description': 'what did the fish say when it hit a wall? dam.',
+        'location': 'florya, istanbul'
+    },
+    {
+        'name': 'DSCF5830',
+        'title': 'matisse',
+        'description': 'the lion does not concern herself with understanding matisse',
+        'location': 'ataturk kultur merkezi, istanbul'
+    },
+    {
+        'name': 'DSCF6050',
+        'title': 'welcome to ankara',
+        'description': 'ankara is known as cold, depressing. when i stepped outside from the esenboga airport, this was the scene that was waiting for me. i guess what i have heard was pretty accurate',
+        'location': 'esenboga airport, ankara'
+    },
+    {
+        'name': 'DSCF6083',
+        'title': 'ankaralı amca',
+        'description': 'amca commuting using the terrible public transportation of ankara',
+        'location': 'ankara'
+    },
+    {
+        'name': 'DSCF6093',
+        'title': 'ankara_streets_1',
+        'description': 'just a photo of some street of ankara',
+        'location': 'kizilay, ankara'
+    },
+    {
+        'name': 'DSCF6094',
+        'title': 'ankara_streets_2',
+        'description': 'just a photo of some street of ankara',
+        'location': 'kizilay, ankara'
+    },
+    {
+        'name': 'DSCF6361',
+        'title': 'self portrait',
+        'description': 'sima just a few days before removing her lip piercings. felt cool tbh.',
+        'location': 'sabanci university, istanbul'
+    }
+]
